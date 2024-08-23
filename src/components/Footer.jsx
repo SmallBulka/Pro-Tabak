62f58d340f9134e76c3217b5f6b9e426fc0b399b
wagakir <125695677+wagakir@users.noreply.github.com>
2024-08-23 18:10:26 +0500

    updateYandexMaps

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,51 +1,62 @@
-import Whatsapp from '../assets/Whatsapp';
+/* eslint-disable no-irregular-whitespace */
+import Whatsapp from "../assets/Whatsapp";
 
 function Footer() {
   return (
-    <footer className=' mt-[80px] xl:mt-[100px]  relative z-20 bg-[#191d21] p-8 '>
-      <div className='container mx-auto px-0'>
+    <footer className=" mt-[80px] xl:mt-[100px]  relative z-20 bg-[#191d21] p-8 ">
+      <div className="container mx-auto px-0">
         <div className="flex flex-col xl:flex-row xl:gap-[100px] xl:mb-[15px]">
-          <div className=" w-full max-w-[400px] mx-auto mb-8 
+          <div
+            className=" w-full max-w-[400px] mx-auto mb-8 
           text-center xl:text-left "
           >
-          <a className="flex justify-center xl:justify-start mb-8" href="">
-          <div className=' text-4xl  text-slate-100 font-rubik '>Pro_Табак</div></a>
-          <p className="mb-8 text-xl text-[#fffa]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque</p>
-          <ul className='text-pretty flex gap-[54px] justify-center xl:justify-start'>
-            <li className='flex gap-3 items-center'>
-              <a href=""><Whatsapp/></a>
-              <p className='text-[#fffa]'>+7 (919) 948 79 38</p>
-            </li>
-          </ul>
+            <a className="flex justify-center xl:justify-start mb-8" href="">
+              <div className=" text-4xl  text-slate-100 font-rubik ">
+                Pro_Табак
+              </div>
+            </a>
+            <p className="mb-8 text-xl text-[#fffa]">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Doloremque
+            </p>
+            <ul className="text-pretty flex gap-[54px] justify-center xl:justify-start">
+              <li className="flex gap-3 items-center">
+                <a href="">
+                  <Whatsapp />
+                </a>
+                <p className="text-[#fffa]">+7 (999) 999 99 99</p>
+              </li>
+            </ul>
           </div>
-          
-          
-          <div className='max-w-[260px] mx-auto xl:mx-0 text-[#fffa]'>
-            <p className='mb-3'>Адреса магазинов</p>
-            <div className='mb-3 flex flex-col gap-5 '>
+
+          <div className="max-w-[260px] mx-auto xl:mx-0 text-[#fffa]">
+            <p className="mb-3">Адреса магазинов</p>
+            <div className="mb-3 flex flex-col gap-5 ">
               <p>​Ул. Федюнинского, 55</p>
               <p>​Ул. Герцена, 95</p>
-              <a href="#" className=" text-[#ffce00]">Задать вопрос</a>
-              <a href="#" className="text-[#ffce00]">Заказать звонок</a>
-              <a href="#" className="text-[#ffce00]">Запросить оптовый прайс</a>
+              <a href="#" className=" text-[#ffce00]">
+                Задать вопрос
+              </a>
+              <a href="#" className="text-[#ffce00]">
+                Заказать звонок
+              </a>
+              <a href="#" className="text-[#ffce00]">
+                Запросить оптовый прайс
+              </a>
             </div>
-
           </div>
-          <div className='flex items-center m-auto'>
-          <div className="flex h-[90px] w-[310px]  bg-white border-8 border-black">
-            <p className='text-balance text-center font-bold p-1'>МИНЗДРАВ ПРЕДУПРЕЖДАЕТ:
-            КУРЕНИЕ ВРЕДИТ ВАШЕМУ ЗДОРОВЬЮ!</p>
-
-</div>
-</div>
-
+          <div className="flex items-center m-auto">
+            <div className="flex h-[90px] w-[310px]  bg-white border-8 border-black">
+              <p className="text-balance text-center font-bold p-1">
+                МИНЗДРАВ ПРЕДУПРЕЖДАЕТ: КУРЕНИЕ ВРЕДИТ ВАШЕМУ ЗДОРОВЬЮ!
+              </p>
+            </div>
           </div>
-          <hr></hr>
         </div>
-     
+        <hr></hr>
+      </div>
     </footer>
-    
-  )
+  );
 }
 
-export default Footer
+export default Footer;
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,47 +1,70 @@
-
-
 function Form() {
-  return(
-  <div className=' mt-[80px] xl:mt-[100px]  relative z-20  xl:flex justify-center '>
-  <div className='relative xl:w-[1110px] lg:h-[400px] bg-[#191d21] rounded-[20px] overflow-hidden
+  return (
+    <div className=" mt-[80px] xl:mt-[100px]  relative z-20  xl:flex justify-center ">
+      <div
+        className="relative xl:w-[1110px] lg:h-[400px] bg-[#191d21] rounded-[20px] overflow-hidden
      before:transform before:skew-y-[345deg] before:transition-[0.5s]
      card hover:before:top-[-70%] hover:before:transform hover:before:skew-y-[390deg] p-6
-    '>
-      <h2 className='h2 text-center pt-6'>Связаться с нами</h2>
-      <p className='text-center text-white'>Свяжитесь с нами, будем рады помочь вам по любым вопросам.</p>
-      
-      <div>
-          <form className='lg:flex md:flex-wrap md:justify-center pt-10 items-center '>
-        <div className='mb-6 mr-[100px]'>
-          <label className='text-white p-4' htmlFor="name">Имя</label>
-          <input className='rounded-[20px] bg-[#2D343C] w-[300px] h-[40px]' type="text" id="name" name="name"  />
-        </div>
-  
-        <div className='mb-6'>
-          <label className='text-white p-4' htmlFor="email">Email</label>
-          <input className='rounded-[20px] bg-[#2D343C] w-[300px] h-[40px]' type="email" id="email" name="email"  />
-        </div>
-  
-        <div className='mb-6 pr-11 md:flex items-center'>
-          <label className='text-white p-4' htmlFor="message">Сообщение</label>
-          <textarea className='rounded-[20px] bg-[#2D343C] w-[400px] h-[80px]' id="message" name="message" />
-        </div>
-  
-        <button className='btn-primary mx-auto xl:mx-0 h-[50px] px-[40px] rounded-[18px]' type="submit">Отправить сообщение</button>
-      </form>
-      
-      </div>
-      <hr></hr>
-      <div className='md:flex justify-center gap-[150px] pt-7 text-white h3'>
-      <p>+7 (919) 948 79 38</p>
-      <p>dreamy27@yandex.ru</p>
-      </div>
-      </div>
-  
-  
-  </div>
-  )
+    "
+      >
+        <h2 className="h2 text-center pt-6">Связаться с нами</h2>
+        <p className="text-center text-white">
+          Свяжитесь с нами, будем рады помочь вам по любым вопросам.
+        </p>
 
+        <div>
+          <form className="lg:flex md:flex-wrap md:justify-center pt-10 items-center ">
+            <div className="mb-6 mr-[100px]">
+              <label className="text-white p-4" htmlFor="name">
+                Имя
+              </label>
+              <input
+                className="rounded-[20px] bg-[#2D343C] w-[300px] h-[40px]"
+                type="text"
+                id="name"
+                name="name"
+              />
+            </div>
+
+            <div className="mb-6">
+              <label className="text-white p-4" htmlFor="email">
+                Email
+              </label>
+              <input
+                className="rounded-[20px] bg-[#2D343C] w-[300px] h-[40px]"
+                type="email"
+                id="email"
+                name="email"
+              />
+            </div>
+
+            <div className="mb-6 pr-11 md:flex items-center">
+              <label className="text-white p-4" htmlFor="message">
+                Сообщение
+              </label>
+              <textarea
+                className="rounded-[20px] bg-[#2D343C] w-[400px] h-[80px]"
+                id="message"
+                name="message"
+              />
+            </div>
+
+            <button
+              className="btn-primary mx-auto xl:mx-0 h-[50px] px-[40px] rounded-[18px]"
+              type="submit"
+            >
+              Отправить сообщение
+            </button>
+          </form>
+        </div>
+        <hr></hr>
+        <div className="md:flex justify-center gap-[150px] pt-7 text-white h3">
+          <p>+7 (999) 999 99 99</p>
+          <p>protabak@mail.ru</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Form;
