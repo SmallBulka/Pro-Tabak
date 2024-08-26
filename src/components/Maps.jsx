b2f5d580234812197ef7e5f939e89aed67486a89
wagakir <125695677+wagakir@users.noreply.github.com>
2024-08-26 18:23:08 +0300

    update react yandex maps

--- src/components/Maps.jsx
+++ src/components/Maps.jsx
@@ -1,24 +1,24 @@
-import { YMaps, Map, Placemark } from 'react-yandex-maps';
+import { YMaps, Map, Placemark } from "@pbe/react-yandex-maps";
 
-  function Maps() {
-    
-    return (
-      <div>
-        <hr className='br m-auto'></hr>
-        <h2 className='h2 p-10  flex justify-center'>Где купить?</h2>
-      <div className='flex justify-center '>
-      
-          
-      <YMaps>
-        <Map width='1120px' height='360px' defaultState={{ center: [57.146720, 65.545090], zoom: 11 }}>
-          <Placemark geometry={[57.148039, 65.543221]} />
-          <Placemark geometry={[57.107020, 65.559319]} />
-        </Map>
-      </YMaps>
-      
+function Maps() {
+  return (
+    <div>
+      <hr className="br m-auto"></hr>
+      <h2 className="h2 p-10  flex justify-center">Где купить?</h2>
+      <div className="flex justify-center ">
+        <YMaps>
+          <Map
+            width="1120px"
+            height="360px"
+            defaultState={{ center: [57.14672, 65.54509], zoom: 11 }}
+          >
+            <Placemark geometry={[57.148039, 65.543221]} />
+            <Placemark geometry={[57.10702, 65.559319]} />
+          </Map>
+        </YMaps>
       </div>
-      </div>
-    );
-  }
-  
-  export default Maps;
+    </div>
+  );
+}
+
+export default Maps;
