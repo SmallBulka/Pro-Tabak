79957652471594da7384ff9d3b6dcdd6f2d3c1b9
wagakir <125695677+wagakir@users.noreply.github.com>
2024-08-29 13:35:39 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -56,7 +56,7 @@ function App() {
                 <div className="w-full p-[20px] lg:p-p-10 ">
                   <Card
                     title="Сигареты"
-                    imageImg="../img/cigaronne.webp"
+                    imageImg="img/cigaronne.webp"
                     scrollToMiniMap={scrollToMiniMap}
                   />
                 </div>
@@ -66,7 +66,7 @@ function App() {
                 <div className="w-full p-[20px] md:p-p-10 ">
                   <Catalog
                     title="Электронные сигареты"
-                    imageImg="../img/34194.970.png"
+                    imageImg="img/34194.970.png"
                     scrollToMiniMap={scrollToMiniMap}
                   />
                 </div>
@@ -76,7 +76,7 @@ function App() {
                 <div className="w-full p-[20px] md:p-p-10">
                   <Catalog
                     title="Самокрутоки и трубки"
-                    imageImg="../img/middle.png"
+                    imageImg="img/middle.png"
                     scrollToMiniMap={scrollToMiniMap}
                   />
                 </div>
@@ -85,7 +85,7 @@ function App() {
                 <div className="w-full p-[20px] md:p-p-10">
                   <Catalog
                     title=" Стики"
-                    imageImg="../img/iqos.png"
+                    imageImg="img/iqos.png"
                     scrollToMiniMap={scrollToMiniMap}
                   />
                 </div>
@@ -94,21 +94,21 @@ function App() {
                 <div className="w-full p-[20px] md:p-p-10">
                   <Catalog
                     title="Всё для кальянов"
-                    imageImg="../img/fona.png"
+                    imageImg="img/fona.png"
                     scrollToMiniMap={scrollToMiniMap}
                   />
                 </div>
               </div>
               {/* <div className="flex w-1/3 flex-wrap">
       <div className="w-full p-[40px] md:p-p-10">
-      <Catalog title="Папиросы" imageImg="../img/scale_1200.png"/>
+      <Catalog title="Папиросы" imageImg="img/scale_1200.png"/>
       </div>
     </div> */}
               <div className="flex xl:w-1/3 flex-wrap">
                 <div className="w-full lg:pl-0 p-[20px] md:p-p-10">
                   <Catalog
                     title="Сувенирная продукция"
-                    imageImg="../img/4.png"
+                    imageImg="img/4.png"
                     scrollToMiniMap={scrollToMiniMap}
                   />
                 </div>
@@ -117,7 +117,7 @@ function App() {
                 <div className="w-full p-[20px] lg:pl-0  ">
                   <Card
                     title="Папиросы"
-                    imageImg="../img/scale_1200.png"
+                    imageImg="img/scale_1200.png"
                     scrollToMiniMap={scrollToMiniMap}
                   />
                 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     screens: {
-      sm: '640px',
-      md: '768px',
-      lg: '940px',
-      xl: '1200px'
-      
+      sm: "640px",
+      md: "768px",
+      lg: "940px",
+      xl: "1200px",
     },
     extend: {
-      fontFamily:{
+      fontFamily: {
         rubik: ["Rubik Mono One", "monospace"],
         rboto: ["Roboto", "sans-serif"],
         montserra: ["Montserrat", "sans-serif"],
         almontserra: ["Montserrat Alternates", "sans-serif"],
-        raleway: ["Raleway", "sans-serif"]
+        raleway: ["Raleway", "sans-serif"],
       },
       colors: {
-        lighgra: '#D4C17F',
-        placeholder: '#BEBEBE',
-        heroGradient: 'rgba(45, 52, 60, 0.38)',
-        
+        lighgra: "#D4C17F",
+        placeholder: "#BEBEBE",
+        heroGradient: "rgba(45, 52, 60, 0.38)",
       },
       backgroundImage: {
-        'hero': "url('/img/Gag.jpg')"
-        
+        hero: "url('/img/Gag.jpg')",
       },
     },
   },
   plugins: [],
-} 
+};
