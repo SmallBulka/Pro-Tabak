f9d1c064b91c74a51fe266790256c7335f4d330b
wagakir <125695677+wagakir@users.noreply.github.com>
2024-09-26 22:33:41 +0500

    update

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,12 @@
 // eslint-disable-next-line react/prop-types
 function Card({ name, resin, nicotine, imageImg }) {
   return (
-    <div className="block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
-      <a href="#!">
-        <img className="imgGradient" src={imageImg} alt="" />
-      </a>
+    <div className="flex flex-col pt-2 rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
+      <div className="h-[150px] flex justify-center items-center">
+        <a href="#!">
+          <img className="imgGradient" src={imageImg} alt="" />
+        </a>
+      </div>
       <div className="p-6">
         <h5 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
           {name}
