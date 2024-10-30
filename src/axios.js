ef86f876fcecce4b7c26c4c09c5ca22ba0505886
wagakir <125695677+wagakir@users.noreply.github.com>
2024-10-31 01:55:50 +0500

    deploy

--- src/axios.js
+++ src/axios.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "http://147.45.147.116",
+  baseURL: "http://147.45.147.116:4444",
 });
 instance.interceptors.request.use((config) => {
   config.headers.Authorization = window.localStorage.getItem("token");
   return config;
 });
-export const baseURL = "http://147.45.147.116";
+export const baseURL = "http://147.45.147.116:4444";
 export default instance;
