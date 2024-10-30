9fe594b0b5e1e9632ab49bdbad4db5c41a67821c
wagakir <125695677+wagakir@users.noreply.github.com>
2024-10-30 22:55:15 +0500

    update

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+
+const PORT = process.env.PORT || 4444;
+
+const app = express();
+app.use(express.static(__dirname));
+app.use(express.static(path.resolve(__dirname, "build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+app.listen(PORT);
