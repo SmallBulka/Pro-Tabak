0807ec04d214d5818ccba2028bdaa84f1e908fb0
wagakir <125695677+wagakir@users.noreply.github.com>
2024-10-31 07:59:27 +0500

    update

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-import express from "express";
-import path from "path";
-import process from "process";
+// import express from "express";
+// import path from "path";
+// import process from "process";
 
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
-const PORT = process.env.PORT || 8080;
+// const PORT = process.env.PORT || 8080;
 
-const app = express();
-app.use(express.static(__dirname));
-app.use(express.static(path.resolve(__dirname, "dist")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
-});
-app.listen(PORT);
+// const app = express();
+// app.use(express.static(__dirname));
+// app.use(express.static(path.resolve(__dirname, "dist")));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "dist", "index.html"));
+// });
+// app.listen(PORT);
