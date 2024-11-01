3676984e5ec453a17fcfb383a1ca02aa8c5443d7
wagakir <125695677+wagakir@users.noreply.github.com>
2024-11-01 16:41:05 +0500

    update

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://147.45.147.116/api",
+  baseURL: "http://api.xn----8sbad0ctgiu.xn--p1ai",
 });
 axios.defaults.headers.post["Content-Type"] = "application/json;charset=utf-8";
 axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
@@ -9,5 +9,5 @@ instance.interceptors.request.use((config) => {
   config.headers.Authorization = window.localStorage.getItem("token");
   return config;
 });
-export const baseURL = "http://147.45.147.116/api";
+export const baseURL = "http://api.xn----8sbad0ctgiu.xn--p1ai";
 export default instance;
